243602b4d1ef1203a87e12c02606ef6e3e5e9599
ybadmus <yusif.badmus@gmail.com>
2021-01-21 23:09:03 +0000

    add my_each function

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,3 +1,19 @@
 module Enumerable
+
+    def my_each
+
+        return to_enum(:self) unless block_given? 
+             
+        i = 0
+
+        while i < to_a.length
+            yield to_a[i]
+            i += 1
+        end
+        self
+
+    end
+
     
+
 end
