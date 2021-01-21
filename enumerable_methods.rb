14e48ba708d3482622d54eff3f4576d11b5d3d03
ybadmus <yusif.badmus@gmail.com>
2021-01-21 23:09:55 +0000

    add my_each_with_index

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -14,6 +14,18 @@ module Enumerable
 
     end
 
-    
+    def my_each_with_index
+
+        return to_enum(:self) unless block_given?
+
+        i = 0
+
+        while i < to_a.length
+            yield to_a[i], i
+            i += 1
+        end
+        self
+
+    end
 
 end
