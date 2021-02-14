abe0293575a2e99d636764ace32ad53f42cce700
ybadmus <yusif.badmus@gmail.com>
2021-02-14 00:54:42 +0000

    test passes for all scenarios of my_all method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -4,7 +4,7 @@ describe Enumerable do
   describe '#my_each' do
     let(:list) { %w[Accra Lagos Lome Cotonou Bamako] }
 
-    it "returns a list of all items in an Array" do
+    it "Returns a list of all items in an Array" do
       res = []
       list.my_each { |item| res << item }
       expect(res).to eq(list)
@@ -23,7 +23,7 @@ describe Enumerable do
         }
       end
 
-    it "lists all the items in the Array and their corresponding index" do
+    it "Lists all the items in the Array and their corresponding index" do
       
       output = Hash.new
       list.my_each_with_index { |item, index| output[:item] = index }
@@ -33,9 +33,44 @@ describe Enumerable do
 
   describe '#my_select' do
     it "Returns an array containing all elements of enum for which the given block returns a true value." do
-      output = [1,2,3,4,5].select { |num|  num.even?  } 
       res = [2, 4]
+      output = [1,2,3,4,5].select { |num|  num.even?  } 
       expect(res.sum).to eq(output.sum)
     end
+  end
+
+  describe '#my_all' do
+    it "1. The method returns true if the block never returns false or nil" do
+      output = %w[ant bear cat].all? { |word| word.length >= 3 }
+      expect(output).to be_truthy
+    end
+
+    it "2. The method returns true if the block never returns false or nil" do
+      output = %w[ant bear cat].all? { |word| word.length >= 4 }
+      expect(output).to be_falsy
+    end
+
+    it "3. The method returns true if the block never returns false or nil" do
+      output = [nil, true, 99].all?    
+      expect(output).to be_falsy
+    end
+
+    it "4. The method returns true if the block never returns false or nil" do
+      output = [].all?             
+      expect(output).to be_truthy
+    end
+
+    it "1. Returns whether pattern === element for every collection member." do
+      output = %w[ant bear cat].all?(/t/)  
+      expect(output).to be_falsy
+    end
+
+    it "2. Returns whether pattern === element for every collection member." do
+      output = [1, 2i, 3.14].all?(Numeric)   
+      expect(output).to be_truthy
+    end
+
+    
+    
   end
 end
